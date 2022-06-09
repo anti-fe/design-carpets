3aa74ee25956c8f1ad74f68da830f17395d65018
anti <raylyangosha@gmail.com>
2022-06-09 15:35:56 +0300

    added first screen section with adaptive

--- scripts/burgerMenu.js
+++
@@ -1,3 +0,0 @@
-export default function burgerMenu(burgerMenu) {
-    burgerMenu.classList.toggle('burger-menu_active')
-}
--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,14 @@
-import setBurgerMenu from './burgerMenu.js';
-import scrollHeader from './scrollHeader.js';
+
 
 const burgerMenuBtn = document.querySelector('.burger-menu__close-btn'),
     burgerMenu = document.querySelector('.burger-menu');
 const header = document.querySelector('.header');
 
+//Открытие бургер меню
 burgerMenuBtn.addEventListener('click', ()=>{
-    setBurgerMenu(burgerMenu);
+    burgerMenu.classList.toggle('burger-menu_active')
 })
+//Уменьшение header при скроле
 window.addEventListener('scroll', (e)=>{
     window.scrollY >= 100 ? header.classList.add('header_active') : header.classList.remove('header_active');
 })
